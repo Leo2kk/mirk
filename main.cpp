88b3f8f474e08d1c510fb910ae7952136f56e826
Leo2kk <50532532+Leo2kk@users.noreply.github.com>
2019-05-23 22:07:06 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,45 +8,17 @@ ifstream fon("nr2.txt");
 
 int main()
 {
-    int x , y , v[10005] , v1[10005] , k , k1;
-    int i , j , v_t[10005] , k2;
-    k = 0;
-    k1 = 0;
-    k2 = 0;
+    int x , y;
+    fon >> y;
     while(fin>>x)
     {
-        if(x%2==0)
-        {
-            v[++k] = x;
+        if(x==y&&x%2==0)
+            cout << x << " ";
+        else while(x>y&&!fon.eof()){
+            fon >> y;
+            if(x==y&&x%2==0)
+                cout << x << " ";
         }
     }
-    while(fon >> y)
-    {
-        if(y%2==0)
-        {
-            v1[++k1] = y;
-        }
-    }
-    i = 1;
-    j = 1;
-    while(i<=k&&j<=k1)
-    {
-        if(v[i]>v1[j])
-        {
-            j++;
-        }
-        else if(v[i]<v1[j])
-        {
-            i++;
-        }
-        else if(v[i]==v1[j])
-        {
-            v_t[++k2] = v[i];
-            i++;
-            j++;
-        }
-    }
-    for(i=1;i<=k2;i++)
-        cout << v_t[i] << " ";
     return 0;
 }
